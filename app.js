64ab60a65aa72d1f5cff39471812cfbd34bef2a0
gfunk77 <faraci77@gmail.com>
2022-11-05 14:55:30 -0400

    add content n Vue

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const app = Vue.createApp({
   data() {
     return {
       name: "Old Fashioned",
+      ingredients: "Ingredients",
       bourbon: "2 oz. Bourbon",
       syrup: "1/4 oz. simple syrup",
       bitters: "2 dashes Angostura Bitters",
       water: "Splash of water",
       orange: "Orange Peel",
+      pic: "cocktail.jpg",
+      link: "https://www.liquor.com/recipes/bourbon-old-fashioned/",
     };
   },
 });
