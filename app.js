ce9b04eb9ca4b49970e92024ce303608b4ec50be
gfunk77 <faraci77@gmail.com>
2022-11-05 15:44:38 -0400

    add cherries

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
       syrup: "1/4 oz. simple syrup",
       bitters: "2 dashes Angostura Bitters",
       water: "Splash of water",
+      cherry: "Maraschino Cherries",
       orange: "Orange Peel",
       pic: "cocktail.jpg",
       link: "https://www.liquor.com/recipes/bourbon-old-fashioned/",
